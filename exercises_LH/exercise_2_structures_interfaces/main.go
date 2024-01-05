9c34890473f7cd09f66b1e64e0600ff62b091a9d
Diego Urrego <diego.urrego@mercadolibre.com.co>
2024-01-05 01:45:04 -0500

    feat: exercise 2 of structures and interfaces completed. Factory method implemented succesfully.

--- exercises_LH/exercise_2_structures_interfaces/main.go
+++ exercises_LH/exercise_2_structures_interfaces/main.go
@@ -1,26 +1,61 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
-// Registro de estudiantes
-
-type Student struct {
-	Name              string
-	Lastname          string
-	DNI               string
-	DateOfInscription time.Time
+type Product interface {
+	Price(productPrice float64) float64
 }
 
-func (e Student) GetStudentDetail() {
-	fmt.Printf("Name: %s\n Lastname: %s\n DNI: %s\n Date of inscription: %s",
-		e.Name, e.Lastname, e.DNI, e.DateOfInscription)
+// Productos
+
+type Small struct {
+}
+
+func (s Small) Price(productPrice float64) float64 {
+	return productPrice
+}
+
+type Medium struct {
+}
+
+func (m Medium) Price(productPrice float64) float64 {
+	return productPrice + (productPrice * 0.03)
+}
+
+type Large struct {
+}
+
+func (l Large) Price(productPrice float64) float64 {
+	var shippingCost = 2500.0
+	return productPrice + (productPrice * 0.06) + shippingCost
+}
+
+func Factory(size uint) Product {
+	switch size {
+	case 1:
+		return Small{}
+	case 2:
+		return Medium{}
+	case 3:
+		return Large{}
+	default:
+		return nil
+	}
 }
 
 func main() {
-	student := Student{Name: "Ricardo", Lastname: "Gonzalez", DNI: "2420131022",
-		DateOfInscription: time.Date(2013, 02, 04, 0, 0, 0, 0, time.UTC)}
-	student.GetStudentDetail()
+	validSizeMessage := "You must enter a valid size"
+
+	fmt.Println("Digita un número de tamaño de producto:")
+	fmt.Println("\t 1: Small 2: Medium 3: Large")
+	var size uint
+	_, err := fmt.Scanln(&size)
+	if err != nil {
+		panic(validSizeMessage)
+	}
+	if size > 3 || size == 0 {
+		panic(validSizeMessage)
+	}
+	product := Factory(size)
+	fmt.Printf("The product has a price of: %v\n", product.Price(43500))
 }
